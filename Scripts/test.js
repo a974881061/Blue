35d18f7a28b6fa27708d42319638f0787f1ef770
a974881061 <60080834+a974881061@users.noreply.github.com>
2022-04-16 19:22:06 +0800

    Update test.js

--- Scripts/test.js
+++ Scripts/test.js
@@ -86,10 +86,23 @@ function ttlyck() {
 //版块
 function bankuai(timeout = 0) {
     return new Promise((resolve) => {
-
+        //资产获取
         let url = {
-            url: ``,
-            headers: JSON.parse(ttlyhd),
+            url: `https://app.fmcc.com.cn/bass-bonusServer/getUserTotalInfo`,
+            headers: {
+                'Host': 'app.fmcc.com.cn',
+                'Accept': 'application/json, text/plain, */*',
+                'X-Tingyun':'c=B|0eimFYky29Y;x=7dc7d8623e66460b',
+                'Accept-Encoding':'gzip, deflate, br',
+                'Accept-Language':'zh-CN,zh-Hans;q=0.9',
+                'Connection':'keep-alive',
+                'H5_TOKEN':ttlyhd,
+                'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;fj-bmsh;fontSizeScale1.00',
+                'ms':'PZU2LBV22KLOWRUDUWJWEXCIXM',
+                'Referer':'https://app.fmcc.com.cn/bass-bountyH5/home',
+                'hc':'599',
+                'Cookie':'sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22599500184154566%22%2C%22first_id%22%3A%2217fddc62d7dcc2-08a3daa2ccf0c28-7c7d2b14-329160-17fddc62d7e25b2%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfbG9naW5faWQiOiI1OTk1MDAxODQxNTQ1NjYiLCIkaWRlbnRpdHlfY29va2llX2lkIjoiMTdmZGRjNjJkN2RjYzItMDhhM2RhYTJjY2YwYzI4LTdjN2QyYjE0LTMyOTE2MC0xN2ZkZGM2MmQ3ZTI1YjIifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22599500184154566%22%7D%2C%22%24device_id%22%3A%2217fddc62d7dcc2-08a3daa2ccf0c28-7c7d2b14-329160-17fddc62d7e25b2%22%7D; H5_TOKEN=446F2757AF027D57AA7F1C1491FDF292FD50CE526897E2D9104DC1F3A23FE02D3C25F8262F77F825; hc=599; ms=PZU2LBV22KLOWRUDUWJWEXCIXM; uid=599500184154566; WT_FPC=id=23985e4934fdce6703f1648693352596:lv=1648693352596:ss=1648693352596; sajssdk_2015_cross_new_user=1'
+            },
         }
 
         $.get(url, async (err, resp, data) => {
@@ -97,11 +110,13 @@ function bankuai(timeout = 0) {
 
                 data = JSON.parse(data)
 
-                if (data.status == 0) {
-
+                if (data.code == 0000) {
+                    let bonusLeftNum = data.data.bonusLeftNum
+                    $.log(`总奖励金为:${data.data.bonusTotalNum},当前奖励金为：${bonusLeftNum},已减排${data.data.carbonTotalNum}g,低碳${data.data.carbonDayCount}天`)
+                    return bonusLeftNum
 
                 } else {
-
+                    $.log('获取资产失败!!!')
 
                 }
             } catch (e) {
