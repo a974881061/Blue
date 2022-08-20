f6e9493d98c8a18926470159d4977a7c58791c55
a974881061 <60080834+a974881061@users.noreply.github.com>
2022-08-20 17:33:38 +0800

    Create today.js

---
+++ Scripts/today.js
@@ -0,0 +1,480 @@
+var obj = JSON.parse($response.body); 
+// 可以合并一句带过
+
+obj = {
+    "message" : "SUCCESS",
+    "datas" : {
+      "currApprover" : null,
+      "detail" : {
+        "approverName" : "艾赛江·扎衣尔",
+        "outStatus" : true,
+        "id" : "02fba7d4482a424998db0a4f65053984",
+        "sex" : "男",
+        "disclaimers" : "本人承诺填写的信息真实有效，并对本次提交请假申请的信息真实性负责。",
+        "lastOut" : null,
+        "totalDays" : "60天7小时",
+        "mustOut" : 1,
+        "isResumptionSelf" : null,
+        "leaveType" : "暑假离校学生情况登记",
+        "dstStatus" : "6",
+        "isIotReport" : 0,
+        "reportLocation" : null,
+        "actStatus" : "6",
+        "ccInfo" : null,
+        "endDate" : "2022-09-02 22:16",
+        "urgencyMobile" : "13164842902",
+        "status" : "2",
+        "reportComment" : null,
+        "pressStatus" : false,
+        "isEarlyEnd" : "0",
+        "userId" : "3196020062",
+        "locationType" : 0,
+        "isOverdue" : true,
+        "reportLatitude" : null,
+        "approverWid" : "90618265",
+        "logInfos" : [
+          {
+            "status" : "1",
+            "approverWid" : null,
+            "nodeName" : null,
+            "approverName" : null,
+            "leaveId" : "02fba7d4482a424998db0a4f65053984",
+            "extendId" : null,
+            "approveOption" : null,
+            "nodeType" : null,
+            "createTime" : "2022-07-02 15:17:23",
+            "delayStatus" : null
+          },
+          {
+            "status" : "3",
+            "approverWid" : null,
+            "nodeName" : null,
+            "approverName" : null,
+            "leaveId" : "02fba7d4482a424998db0a4f65053984",
+            "extendId" : null,
+            "approveOption" : null,
+            "nodeType" : null,
+            "createTime" : "2022-07-02 15:45:03",
+            "delayStatus" : null
+          }
+        ],
+        "actEndTime" : "09-02 22:16",
+        "actEndTimeDesc" : "60天7小时",
+        "reportLongitude" : null,
+        "leaveCreateTime" : "07-02 15:17",
+        "destination" : "福建省/漳州市/平和县/山格镇高楼7号",
+        "leaveReason" : "回家过个暑假过个暑假",
+        "reportAttach" : [
+  
+        ],
+        "startDate" : "2022-07-04 15:16",
+        "approverOpinion" : "",
+        "startTime" : "07-04 15:16",
+        "userName" : "许宇俊",
+        "reportStatus" : null,
+        "reportLogs" : [
+  
+        ],
+        "applyLocation" : {
+          "locationType" : 0,
+          "latitude" : "26.085506981070726",
+          "longitude" : "119.24190072574089",
+          "address" : "福建省福州市仓山区同安路"
+        },
+        "endTime" : "09-02 22:16",
+        "gender" : "1",
+        "approverId" : "000Q818017",
+        "applyAttach" : [
+  
+        ],
+        "leaveName" : "暑假离校学生情况登记",
+        "userWid" : null,
+        "createTime" : "07-02 15:17",
+        "studentMobile" : "15259607976",
+        "leaveHour" : "86820",
+        "dormitoryInfo" : "-",
+        "approvers" : [
+          {
+            "status" : "1",
+            "handled" : true,
+            "approveNode" : [
+              {
+                "nodeIndex" : null,
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : null,
+                "approverName" : null,
+                "approveType" : null,
+                "approveOption" : null,
+                "approverWid" : null,
+                "nextApproverIndex" : null,
+                "nodeWid" : null,
+                "nodeName" : null,
+                "createTime" : "2022-07-02 15:17:23",
+                "approvers" : [
+  
+                ],
+                "delayStatus" : null,
+                "approverId" : null,
+                "status" : "1"
+              }
+            ],
+            "approver" : {
+              "userWid" : "1010664769",
+              "userId" : "3196020062",
+              "userName" : "许宇俊"
+            },
+            "level" : null,
+            "approveType" : null,
+            "approveOption" : null,
+            "createTime" : "07-02 15:17",
+            "delayStatus" : null
+          },
+          {
+            "status" : "3",
+            "handled" : true,
+            "approveNode" : [
+              {
+                "nodeIndex" : "0",
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : "1",
+                "approverName" : "艾赛江·扎衣尔",
+                "approveType" : null,
+                "approveOption" : "",
+                "approverWid" : "90618265",
+                "nextApproverIndex" : null,
+                "nodeWid" : "1001",
+                "nodeName" : "校级管理员",
+                "createTime" : "2022-07-02 15:45:03",
+                "approvers" : [
+                  {
+                    "userWid" : "32866",
+                    "userId" : "42527621806",
+                    "userName" : "admin"
+                  },
+                  {
+                    "userWid" : "90577544",
+                    "userId" : "ampadmin",
+                    "userName" : "潘建"
+                  },
+                  {
+                    "userWid" : "90615083",
+                    "userId" : "000l110002",
+                    "userName" : "李园"
+                  },
+                  {
+                    "userWid" : "90615163",
+                    "userId" : "000L813015",
+                    "userName" : "黄瑞章"
+                  },
+                  {
+                    "userWid" : "90615565",
+                    "userId" : "000L817074",
+                    "userName" : "江冰晶"
+                  },
+                  {
+                    "userWid" : "90616114",
+                    "userId" : "000Q070083",
+                    "userName" : "刘婷"
+                  },
+                  {
+                    "userWid" : "90616290",
+                    "userId" : "000Q091057",
+                    "userName" : "陈菁"
+                  },
+                  {
+                    "userWid" : "90616359",
+                    "userId" : "000Q100076",
+                    "userName" : "郑智彬"
+                  },
+                  {
+                    "userWid" : "90616650",
+                    "userId" : "000Q111100",
+                    "userName" : "陈亮"
+                  },
+                  {
+                    "userWid" : "90616718",
+                    "userId" : "000Q120041",
+                    "userName" : "曹伟"
+                  },
+                  {
+                    "userWid" : "90616896",
+                    "userId" : "000Q140001",
+                    "userName" : "刘键政"
+                  },
+                  {
+                    "userWid" : "90616916",
+                    "userId" : "000Q141019",
+                    "userName" : "陈月霄"
+                  },
+                  {
+                    "userWid" : "90617055",
+                    "userId" : "000Q161001",
+                    "userName" : "余建晖"
+                  },
+                  {
+                    "userWid" : "90617160",
+                    "userId" : "000Q271007",
+                    "userName" : "陈潜"
+                  },
+                  {
+                    "userWid" : "90617161",
+                    "userId" : "000Q271009",
+                    "userName" : "胡静"
+                  },
+                  {
+                    "userWid" : "90617549",
+                    "userId" : "000Q811006",
+                    "userName" : "龚琛琛"
+                  },
+                  {
+                    "userWid" : "90617666",
+                    "userId" : "000Q812083",
+                    "userName" : "苏新华"
+                  },
+                  {
+                    "userWid" : "90617778",
+                    "userId" : "000Q813099",
+                    "userName" : "张隆平"
+                  },
+                  {
+                    "userWid" : "90617889",
+                    "userId" : "000Q814101",
+                    "userName" : "方万春"
+                  },
+                  {
+                    "userWid" : "90618157",
+                    "userId" : "000Q816059",
+                    "userName" : "陈杰"
+                  },
+                  {
+                    "userWid" : "90618160",
+                    "userId" : "000Q816062",
+                    "userName" : "郑江枫"
+                  },
+                  {
+                    "userWid" : "90618260",
+                    "userId" : "000Q818012",
+                    "userName" : "娜孜热·艾尔肯"
+                  },
+                  {
+                    "userWid" : "90618265",
+                    "userId" : "000Q818017",
+                    "userName" : "艾赛江·扎衣尔"
+                  },
+                  {
+                    "userWid" : "91109423",
+                    "userId" : "000Q819018",
+                    "userName" : "赵壮"
+                  },
+                  {
+                    "userWid" : "91109430",
+                    "userId" : "000Q819025",
+                    "userName" : "尤达"
+                  },
+                  {
+                    "userWid" : "91118880",
+                    "userId" : "000Q819028",
+                    "userName" : "克拉热·艾尼瓦尔"
+                  },
+                  {
+                    "userWid" : "91764640",
+                    "userId" : "000Q820046",
+                    "userName" : "李娟"
+                  },
+                  {
+                    "userWid" : "91895479",
+                    "userId" : "000Q821017",
+                    "userName" : "陈珊"
+                  },
+                  {
+                    "userWid" : "92277704",
+                    "userId" : "000Q822023",
+                    "userName" : "张春林"
+                  },
+                  {
+                    "userWid" : "92277705",
+                    "userId" : "000Q822024",
+                    "userName" : "沈明龙"
+                  },
+                  {
+                    "userWid" : "92277706",
+                    "userId" : "000Q822025",
+                    "userName" : "陈钤雯"
+                  }
+                ],
+                "delayStatus" : false,
+                "approverId" : "000Q818017",
+                "status" : "3"
+              },
+              {
+                "nodeIndex" : "0",
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : "1",
+                "approverName" : null,
+                "approveType" : null,
+                "approveOption" : null,
+                "approverWid" : "null",
+                "nextApproverIndex" : null,
+                "nodeWid" : "1002",
+                "nodeName" : "院级管理员",
+                "createTime" : "2022-07-02 15:17:23",
+                "approvers" : [
+                  {
+                    "userWid" : "90617307",
+                    "userId" : "000Q351042",
+                    "userName" : "张明辉"
+                  }
+                ],
+                "delayStatus" : false,
+                "approverId" : null,
+                "status" : null
+              },
+              {
+                "nodeIndex" : "0",
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : "1",
+                "approverName" : null,
+                "approveType" : null,
+                "approveOption" : null,
+                "approverWid" : "null",
+                "nextApproverIndex" : null,
+                "nodeWid" : "1003",
+                "nodeName" : "辅导员",
+                "createTime" : "2022-07-02 15:17:23",
+                "approvers" : [
+  
+                ],
+                "delayStatus" : false,
+                "approverId" : null,
+                "status" : null
+              }
+            ],
+            "approver" : {
+              "userWid" : "90618265",
+              "userId" : "000Q818017",
+              "userName" : "艾赛江·扎衣尔"
+            },
+            "level" : "1",
+            "approveType" : null,
+            "approveOption" : "",
+            "createTime" : "07-02 15:45",
+            "delayStatus" : false
+          }
+        ],
+        "ljDays" : null
+      },
+      "holidayAutoDefineVos" : [
+  
+      ],
+      "condition" : {
+        "shutDown" : false,
+        "allowWithdraw" : 1,
+        "reportTutorialUrl" : "http://catqa.cpdaily.com/2018/10/24/如何销假？/",
+        "reportTutorialTitle" : "如何销假？"
+      },
+      "isExtend" : "1",
+      "nextApproveInfo" : null,
+      "leaveType" : {
+        "mustOut" : 1,
+        "id" : "5557",
+        "code" : 6,
+        "needApproval" : 1,
+        "reportApply" : 0,
+        "enableAttach" : 0,
+        "disableTime" : 0,
+        "reportApplyFlow" : 0,
+        "name" : "暑假离校学生情况登记"
+      },
+      "studentInfo" : {
+        "userId" : "3196020062",
+        "gender" : 1,
+        "userWid" : "1010664769",
+        "userName" : "许宇俊",
+        "major" : "网络工程",
+        "grade" : "2019",
+        "cls" : "19网工2班",
+        "dorm" : "",
+        "photo" : null,
+        "dept" : "计算机与信息学院"
+      },
+      "isPress" : null,
+      "isReport" : "1",
+      "recordStatus" : "6",
+      "notOutReport" : {
+        "setedNotOutReport" : 1,
+        "rule" : 1,
+        "operatorWid" : "90618265",
+        "operatorId" : "000Q818017",
+        "operatorName" : "艾赛江·扎衣尔",
+        "operatorDate" : "2021-12-15 09:54:37"
+      },
+      "nowTime" : "2022-08-20 17:19:53",
+      "termination" : {
+        "terminationObjectName" : null,
+        "terminationDate" : null,
+        "allowTerminate" : 0,
+        "terminationReason" : null,
+        "terminated" : 0
+      },
+      "isAllowedExtend" : true,
+      "leaveExtends" : [
+  
+      ],
+      "reportPlaces" : [
+        {
+          "radius" : "660",
+          "counName" : "admin",
+          "latitude" : "26.090116900080083",
+          "longitude" : "119.24530285000763",
+          "wid" : "551",
+          "address" : "福建农林大学"
+        },
+        {
+          "radius" : "500",
+          "counName" : "admin",
+          "latitude" : "26.06215126760241",
+          "longitude" : "119.18906892510162",
+          "wid" : "552",
+          "address" : "福建农林大学(旗山校区)"
+        },
+        {
+          "radius" : "500",
+          "counName" : "admin",
+          "latitude" : "25.08501182023423",
+          "longitude" : "118.2411949173962",
+          "wid" : "553",
+          "address" : "福建农林大学(安溪校区)"
+        },
+        {
+          "radius" : "380",
+          "counName" : "艾赛江·扎衣尔",
+          "latitude" : "26.08620684431339",
+          "longitude" : "119.24336251010672",
+          "wid" : "814",
+          "address" : "福建农林大学（金山学院）"
+        },
+        {
+          "radius" : "280",
+          "counName" : "艾赛江·扎衣尔",
+          "latitude" : "26.092161143473724",
+          "longitude" : "119.24958776728882",
+          "wid" : "815",
+          "address" : "福建农林大学（东苑）"
+        }
+      ]
+    },
+    "code" : "0"
+  }
+  
+// 有需要全部替换的情况，只需让用户在这里如此操作即可
+
+$done({body:JSON.stringify(obj)});
+// 也一句带过
