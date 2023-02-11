84455e778a122681c1893316b3bf2475e343a3f7
a974881061 <60080834+a974881061@users.noreply.github.com>
2023-02-11 11:54:40 +0800

    Update GetCookies.js

--- QuantumultX/GetCookies.js
+++ QuantumultX/GetCookies.js
@@ -1,124 +1,14 @@
-hostname= www.52pojie.cn,c.tieba.baidu.com,passport.biligame.com,api.m.jd.com,api.kkmh.com,passport.iqiyi.com,ios.baertt.com, kd.youth.cn, daojia.jd.com, www.xiaodouzhuan.cn, nebula.kuaishou.com, draw.jdfcloud.com, e.189.cn, api.weibo.cn, api.inews.qq.com, newsapi.sina.cn,ios.baertt.com, www.mydigit.cn, m.weibo.cn, apiwz.midukanshu.com, api.1sapp.com, frodo.douban.com, luckman.suning.com, passport.suning.com, sign.suning.com, gameapi.suning.com, m.ctrip.com, 110.43.90.61, zt.wps.cn, m-bean.kaola.com, daojia.jd.com, app.nio.com, wxprdapplet.gac-nio.com, node.kg.qq.com, api.dushu.io, pm.m.fenqile.com, maicai.api.ddxq.mobi, group.baicizhan.com, api.everphoto.cn, i.meituan.com, promotion.waimai.meituan.com, wx.10086.cn, www.maomicd.com, m.client.10010.com, act.10010.com, api-takumi.mihoyo.com, m.gdoil.cn, credits.bz.mgtv.com, *.csdn.net, wapside.189.cn, *.acfun.cn, apk.tw, *.bilibili.com, api.dongqiudi.com, *.feng.com, www.flyertea.com, music.163.com, *.m.163.com, user.qunar.com, *.rr.tv, sf-integral-sign-in.weixinjia.net, mcs-mimp-web.sf-express.com, *.smzdm.com, tieba.baidu.com, *.v2ex.com, *.video.qq.com, mobwsa.ximalaya.com, *.you.163.com, *.rrys2020.com, ios.zmzapi.comactivity.m.kuaishou.com,weather-data.apple.com, api.weather.com,superapp.kiwa-tech.com, activity-1.m.duiba.com.cn, h5.bianlifeng.com, as.xiaojukeji.com, common.diditaxi.com.cn, music.163.com, user-api-prd-mx.wandafilm.com, mall.oclean.com, m.weibo.cn, dominos*.shjimang.com, cloud.umetrip.com, account.huami.com,webapi.qmai.cn,air.tb.ele.me,tb.ele.me,h5.ele.me,api.xiaoheihe.cn,qqpet.jwetech.com,proxy.vac.qq.com,j1.pupuapi.com,yuba.douyu.com,weibo.com,*.xmcimg.com,exp.angelalign.com, ngabbs.com, node.kg.qq.com, srv.app.ikea.cn,h5.youzan.com, glados.rocks, api.weibo.com, mapi.weibo.com,club.yili.com,geekhub.com, www.geekhub.com,huiyuan.163.com,cxdng.cpic.com.cn,marketappapi.biyao.com,m.xiaomiyoupin.com,app.ymatou.com,sports.lifesense.com,studygolang.com,m.ctrip.com,i.snssdk.com,is.snssdk.com,ios-fenqu11.lxsjgo.com,m.elong.com, activity.m.duiba.com.cn, icbc1.wlphp.com:8444, wx.17u.cn,commontgw6.reader.qq.com, eventv3.reader.qq.com,wox2019.woxshare.com,jdread-api.jd.com,appsmall.rtmap.com,mobile.app.autohome.com.cn,activity.app.autohome.com.cn,api.sdbao.com,app.oscaches.com,operation-api.jimistore.com,api.momosyb.com,hongbao.youzikuaibao.com,*.zhuishushenqi.com,getconfig-globalapi.zymk.cn,youhui.95516.com,api.yuncheapp.cn, *.snssdk.com, veishop.iboxpay.com, *.huoshan.com, *.amemv.com, ranlv.lvfacn.com,api.sxsjyzm.com,ymz.iphonezhuan.com,dkd-api.dysdk.com,api.hemayoudao.cn,jqb.iphonezhuan.com,api.snail2020.com,api-9f9d25.sz365.cn,m.*.top,minapp.xqrobot.net,qmyd.yichengw.cn,yuedongzu.yichengw.cn,huodong.fanli.com, passport.fanli.com, gw.fanli.com,zhiyou.smzdm.com,www.didapinche.com,api.didialift.com,bosp-api.xiaojukeji.com,as.xiaojukeji.com,manhattan.webapp.xiaojukeji.com,*.diditaxi.com.cn,user-api-prd-mx.wandafilm.com,cms-activity-api-prd-mx.wandafilm.com,fmapp.chinafamilymart.com.cn,luka-graphql.ling.cn,mall.meituan.com,sdk.121827.com,ddstar.palmmob.com,vip.75787.com,ifsapp.pceggs.com, *.pceggs.com,www.ipadview.com,*.reader.yueyouxs.com,lite.sina.cn,xw.mengniu.cn,ms.jr.jd.com,m.xiaomiyoupin.com,kandian.wkandian.com,*.youth.cn,*.hdpyqd.com,*.ulikecam.com,*.zijieapi.com,*.toutiaoapi.com, newvideo.autohome.com.cn,*.fqnovel.com,wzq.tenpay.com,*.kuaishou.com,render.shuqireader.com,ocean.shuqireader.com,api-access.pangolin-sdk-toutiao.com,api.st615.com,api.imkaka.com,api.aiqingyinghang.com,goldcoinnew.zhuishushenqi.com,prodenv.toponegames.cn,
+hostname = user-api.smzdm.com,c.tieba.baidu.com,tiebac.baidu.com,maicai.api.ddxq.mobi,passport.iqiyi.com
 
-#David
-# 中青看点极速版
-v5/article/info url script-request-header http://script.frp.aiuyo.com:8680/scripts/zqkd.js
-Nameless/getTaskBrowse url script-request-header http://script.frp.aiuyo.com:8680/scripts/zqkd.js
-NewTaskIos/getTaskList url script-request-header http://script.frp.aiuyo.com:8680/scripts/zqkd.js
-# 快手
-https://encourage.kuaishou.com/rest/wd/encourage/treasureBox/info url script-request-header http://script.frp.aiuyo.com:8680/scripts/ksdsp20220504vcto/ks.js
-# 快看点
-https:\/\/api\.yuncheapp\.cn\/pearl-incentive\/api\/v1\/task\/intervalAward\/receive url script-request-header http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-https://api.yuncheapp.cn/pearl-incentive/api/v1/task/signIn/* url script-request-header http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-api.yuncheapp.cn/pearl-incentive/api/v1/drinkWater/start url script-request-header http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-https://api.yuncheapp.cn/pearl-incentive/api/v1/bank/home  url script-request-header http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-https://api.yuncheapp.cn/pearl-incentive/api/v1/lotteryTable/homeV2 url script-request-header http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-https://api.yuncheapp.cn/pearl-incentive/api/v1/giftRain/home url script-request-header http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-https://api.yuncheapp.cn/pearl-incentive/api/v1/bank/account/exchange url script-request-body http://script.frp.aiuyo.com:8680/scripts/kkd_ck.js
-# 今日头条
-luckycat/lite/v1/withdraw/withdraw url script-request-body http://script.frp.aiuyo.com:8680/scripts/jrtt.js
-luckycat/lite/v1/activity/done_whole_scene_task url script-request-header http://script.frp.aiuyo.com:8680/scripts/jrtt.js
-v1/task.+(open_treasure_box|excitation_ad) url script-request-body http://script.frp.aiuyo.com:8680/scripts/jrtt.js
-# 闪辆
-https://newvideo.autohome.com.cn/api/homepage/detail url script-request-header http://script.frp.aiuyo.com:8680/scripts/sl.js
-https://newvideo.autohome.com.cn/api/video/rcmfeed url script-request-header http://script.frp.aiuyo.com:8680/scripts/sl.js
-# 番茄小说
-luckycat/novel/v1/task/sign_in/.+ url script-request-header http://script.frp.aiuyo.com:8680/scripts/fqxs.js
-# 签到
-ajax/rldk_h.jsp.cmd=sign url script-request-header http://script.frp.aiuyo.com:8680/scripts/qd.js
-# 剪映
-lv/v1/game/get_task_list url script-request-header http://script.frp.aiuyo.com:8680/scripts/jy20220331cdvde/jy.js
-lv/v1/game/save_progress url script-request-header http://script.frp.aiuyo.com:8680/scripts/jy20220331cdvde/jy.js
-lv/v1/game/receive_credits url script-request-header http://script.frp.aiuyo.com:8680/scripts/jy20220331cdvde/jy.js
-# 书旗小说
-https://ocean.shuqireader.com/api/activity/xapi/signin/v5/signInAction url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/activity/v1/task/reward url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/prizecenter/xapi/prize/manual/receive url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/ url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/activity/v1/activity/pendant/lottery? url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/ad/v1/api/prize/lottery url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/actstage/pendant/lottery/action url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/activity/activity/v1/lottery/draw? url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/activity/activity/v1/lottery/info? url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/ad/adserver/v1/api/getAdInfo? url script-request-header http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/jcollection/collection/iosapi/reading/upload url script-request-body http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-https://ocean.shuqireader.com/api/activity/v1/activity/boxTask? url script-request-header http://script.frp.aiuyo.com:8680/scripts/sqxs.js
-# 抖音极速版
-luckycat/aweme/v1/task/sign_in/detail? url script-request-header http://script.frp.aiuyo.com:8680/scripts/dyjsb.js
-luckycat/aweme/v1/task/done/(read|treasure_task|excitation_ad_treasure_box|excitation_ad)? url script-request-header http://script.frp.aiuyo.com:8680/scripts/dyjsb.js
-# 九章头条
-https://api.st615.com/v2/user/task url script-request-header  http://script.frp.aiuyo.com:8680/scripts/jztt.js
-# 笔趣阁
-/welfare/list url script-request-header http://script.frp.aiuyo.com:8680/scripts/bqg.js
-# 养蜂
-/user/getselfgroupsids url script-request-header http://script.frp.aiuyo.com:8680/scripts/yqlyf.js
-
-#Nobyda
-# 吾爱破解 
-https:\/\/www\.52pojie\.cn\/home\.php\?mod=space url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
-# 百度贴吧 
-https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/BDTieBa-DailyBonus/TieBa.js
-# 哔哩哔哩漫画
-^https:\/\/passport\.biligame\.com\/api\/login\/sso.+?version%22%3A%22(3|4|5) url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/Bilibili-DailyBonus/Manga.js
-# 京东
-https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
-# 快看漫画
-^https:\/\/api\.kkmh\.com\/v\d\/passport\/user url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/KuaiKan-DailyBonus/KKMH.js
-# 爱奇艺 cookie
-^https:\/\/passport\.iqiyi\.com\/apis\/user\/ url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/iQIYI-DailyBonus/iQIYI.js
-
-#Sunert
-# 京东到家 
-https:\/\/daojia\.jd\.com\/client\?_jdrandom=\d{13}&functionId=%2Fsignin url script-request-header https://gitee.com/Sunert/Scripts/raw/master/Task/jddj.js
-# 中国电信套餐 (By @uchvk & @Macsuny)
-^https?:\/\/e\.189\.cn\/store\/user\/package_detail\.do url script-request-header https://gitee.com/Sunert/Scripts/raw/master/Task/telecomInfinity.js
-# 微博签到
-#https:\/\/api\.weibo\.cn\/\d\/users\/show url script-request-header https://raw.githubusercontent.com/Sunert/Scripts/master/Task/weibo.js
-#https:\/\/api\.weibo\.cn\/2\/logservice\/service url script-request-header https://raw.githubusercontent.com/Sunert/Scripts/master/Task/weibo.js
-
-#Peng-YM
-#彩云天气
-https:\/\/((weather-data\.apple)|(api.weather))\.com url script-request-header https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/caiyun.js
-
-#id77
-# 伊利乳品
-^https:\/\/club\.yili\.com\/MALLIFChe\/MCSWSIAPI\.asmx\/Call url script-request-body https://raw.githubusercontent.com/id77/QuantumultX/master/task/yiLi.cookie.js
-
-#iepngs
-# 叮咚买菜
-https:\/\/maicai\.api\.ddxq\.mobi\/user\/checkLogin url script-request-header https://raw.githubusercontent.com/iepngs/Script/master/dingdong/index.js
-# 步数上传
-^https:\/\/sports\.lifesense\.com\/sport_service\/sport\/sport\/uploadMobileStepV2 url script-request-body https://raw.githubusercontent.com/iepngs/Script/master/lxhealth/manual.js
-
-#blackmatrix7
-# 什么值得买
-^https?:\/\/zhiyou\.smzdm\.com\/user$ url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_daily.js
-# ^https?:\/\/user-api\.smzdm\.com\/user_login\/normal$ url script-request-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_daily.js
-# 百度贴吧
+# blackmatrix7
+# 什么值得买每日自动签到
+^https?:\/\/user-api\.smzdm\.com\/checkin$ url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_daily.j
+# 百度贴吧每日自动签到
 ^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/tieba/tieba_signin.js
 ^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/tieba/tieba_signin.js
 ^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/tieba/tieba_signin.js
-# 叮咚买菜
+# 叮咚买菜每日自动签到
 ^https?:\/\/maicai\.api\.ddxq\.mobi\/point\/home\?api_version url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/dingdong/dingdong_checkin.js
-# 饿了么
-^https?:\/\/air\.tb\.ele\.me\/app\/conch-page\/svip-foodie-card\/home url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/eleme/eleme_daily.js
-^https?:\/\/air\.tb\.ele\.me\/app\/conch-page\/svip-home-tasklist-new\/home url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/eleme/eleme_daily.js
-^https?:\/\/tb\.ele\.me\/wow\/alsc\/mod url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/eleme/eleme_daily.js
-^https?:\/\/h5\.ele\.me\/restapi\/biz\.svip_scene\/svip\/engine\/queryTrafficSupply url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/eleme/eleme_daily.js
-# Fa米家每日自动签到
-^https?:\/\/fmapp\.chinafamilymart\.com\.cn\/api\/app\/market\/member\/(signin\/usersign|sign\/current) url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/famijia/famijia_checkin.js
-# Luka每日自动签到
-^https?:\/\/luka\-graphql\.ling\.cn\/graphql\/mobile\?operation=UserInfoWithPendingRobotQuery url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/luka/luka_signin.js
-# 美团买菜每日自动签到
-^https?:\/\/mall\.meituan\.com\/api\/c\/mallcoin\/checkIn\/getWeekCheckInProgressBar\? url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/meituan/maicai_checkin.js
-# 小米有品每日自动签到
-^https?:\/\/m\.xiaomiyoupin\.com\/mtop\/coupon\/redpacket\/get_user_balance url script-request-header https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/obsolete/youpin/youpin_checkin.js
 
-#leaf
-# 咔咔
-https://api.imkaka.com/v1/user/index/ url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/kaka.js 
-#追书神器
-https://goldcoinnew.zhuishushenqi.com/account/profile url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/zssq.js
-
-
-
+# 爱奇艺
+^https:\/\/passport\.iqiyi\.com\/apis\/user\/ url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/iQIYI-DailyBonus/iQIYI.js
